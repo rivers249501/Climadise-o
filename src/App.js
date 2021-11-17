014355084326c4b20b33b07141a4a69593ec761e
rivers249501 <rivers.morales@uppuebla.edu.mx>
2021-11-17 00:12:16 -0600

    cambiar

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
 import React from "react";
-import {
-  BrowserRouter,
-  Switch,
-  Route
-} from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import Home from './pages/Home/Home.js';
-import './App.css';
+import Home from "./pages/Home/Home.js";
+import "./App.css";
 import CityInformation from "./pages/CityInformation/CityInformation.js";
 
 function App() {
   return (
-
-     <BrowserRouter >
-     <Switch > 
-      <Route path="/" exact>
-      <Home/>
-      </Route>
-      <Route path="/:city" exact>
-      <CityInformation/>
-      </Route>
-    </Switch> 
-    
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/:city" exact>
+          <CityInformation />
+        </Route>
+      </Switch>
     </BrowserRouter>
-
   );
 }
 
